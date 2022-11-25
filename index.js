3992ef97e0751ba8dd018714f7884427d1ae2a49
+ <+>
2022-11-25 14:12:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+let activity = "";
+
+function saturdayFun(activity= "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+
+function mondayWork(activity= "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+const wrapAdjective= (visualFlair="*")=>{
+    return function(talent = "special"){
+        return (`You are ${visualFlair}${talent}${visualFlair}!`)
+    }
+   
+}
